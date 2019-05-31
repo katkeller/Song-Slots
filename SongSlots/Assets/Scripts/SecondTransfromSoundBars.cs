b71d9633357158e6261562ffb2e8a7a2d53398a4
Kat Keller <kat.kek.keller@gmail.com>
2019-05-31 12:46:47 -0500

    Fixed audio sample collector bug, implimented both audio visualizers, and tweaked values.

---
+++ SongSlots/Assets/Scripts/SecondTransfromSoundBars.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondTransfromSoundBars : MonoBehaviour
+{
+    [SerializeField]
+    private bool useBuffer;
+
+    [SerializeField]
+    private int bandNumber;
+
+    [SerializeField]
+    private float startScale = .25f, scaleMultiplier = 10.0f;
+
+    void Update()
+    {
+        if (useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (SecondAudioSampleCollector.audioBandBuffer[bandNumber] * scaleMultiplier) + startScale, transform.localScale.z);
+        }
+        else if (!useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (SecondAudioSampleCollector.audioBand[bandNumber] * scaleMultiplier) + startScale, transform.localScale.z);
+        }
+    }
+}
