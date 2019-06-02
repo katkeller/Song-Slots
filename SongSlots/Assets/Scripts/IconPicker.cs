45776c97ee8e79820bc5512530ec434896f4b0c5
Kat Keller <kat.kek.keller@gmail.com>
2019-06-02 16:11:30 -0500

    Started working on the icon selection menu

---
+++ SongSlots/Assets/Scripts/IconPicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconPicker : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] toggleIconImage = new Image[9];
+
+    [SerializeField]
+    private Sprite[] iconSprite = new Sprite[6];
+
+    private void ChangeToggleImage()
+    {
+
+    }
+}
